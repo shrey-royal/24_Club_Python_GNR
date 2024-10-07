8db2ae6d380ef0a48035724cda8ae825941daea5
Shrey K <srkkadia@gmail.com>
2024-10-07 15:56:10 +0530

    String Methods and Tasks

---
+++ Project_0/Basics/String_Methods.py
@@ -0,0 +1,76 @@
+# text = "24_Club_Python_GNR"
+
+# print(text.count('_'))
+# print(text.casefold() == "24_club_python_gnr")
+# print(text.casefold().count('n'))
+# print(text.count("_", 2, 10))
+# print(len(text))
+# print(text.center(15, '-'))
+# print("club_Python_GNR".capitalize())
+
+# print("cafē".encode(encoding='ascii', errors='replace'))
+# print(text.endswith("nR"))
+# print("Hello\tW\torldHello".expandtabs(10))
+
+# print("hello world".find('o', 5, 7))
+# print("python programming".find('prog'))
+
+# text = "Good {}!"
+# print(text.format("Evening"))
+# print("Addition of {} and {} is {}".format(5, 3, (5+3)))
+# print("Subtraction of {0} and -{1} is {2}".format(5, 5+3, 3))
+# print(f"Addition {5} and {3} is {5+3}")
+
+# print("hello world".index('x'))
+# print("HELLO124".isupper())
+# print("hello124".islower())
+# print("hello".isalpha())
+# print("1he2llo12".isalnum())
+# print("1he2llo12".isascii())
+# print("9992399".isdigit())
+# print("9992399".isdecimal())
+# print("9992399".isnumeric())
+# print("var1".isidentifier())
+# print("cafē\n".isprintable())
+# print("  ".isspace())
+# print(" Hello World".istitle())
+# print("Zafar Sir".join("123"))
+# print(" ".join(" Zafar Sir "))
+# print("'", "hello".ljust(10, '-'), "'", sep="")
+# print("-*-*-*-*-*|-*-*-*-*-*-*-*-hello".lstrip('*-'))
+
+# string = "royal technosoft pvt. ltd."
+# table = str.maketrans("ot", "0😊")
+# print(table)        # print the translation table
+# print(string.translate(table)) # print the actual replaced string
+
+# print("royal technosoft".partition("tech"))
+# print("royal technosoft".partition(" "))
+# print("royal technosoft".partition("o"))
+# print("royal technosoft".partition(""))     #ValueError: empty separator
+# print("royal technosoft".partition("@"))
+# print("royal technosoft".partition("ft"))
+#
+# print("royal technosoft".replace("tech", "TECH"))
+# print("royal technosoft".replace("tech", ""))
+
+# print("'", "royal technosoft  m      ".rstrip(" "), "'", sep='')
+# print("royal technosoft".removeprefix("ro"))
+# print("royal technosoft".removesuffix("nosoft"))
+# print("royal technosoft".rfind("o"))
+# print("royal technosoft".rindex("e"))
+# print("'", "royal technosoft".rjust(50), "'", sep="")
+# print("royal technosoft".rsplit("tech"))
+# print("royal technosoft".rpartition(" "))
+
+# print("royal technosoft".startswith("roy"))
+# print("royal Technosoft".split("Tech"))
+# print("Lorem ipsum \ndolor sit amet consectetur \n\nadipisicing elit. Error iusto, quam culpa odit non, quae, facilis perspiciatis praesentium optio possimus nisi totam? ".splitlines())
+# print("^^^^^^^^^^^^^^-^^---$-----^^----^-0----&--------Smit Chaudhary-^^---$-----^^----^-0----&--------^^^^^^^^^^^^".strip("-^$"))
+# print("smIT chauDHarY".swapcase())
+
+# print("mOHak PaTeL".title())
+# print("mOHak PaTeL".upper())
+# print("MOHAK patel".lower())
+print("11001".zfill(8))
+# print("Mohak".zfill(40))
