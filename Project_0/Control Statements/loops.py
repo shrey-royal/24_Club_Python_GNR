9534770cd8a0562ab0a7f690ef41984603b7cfbe
Shrey K <srkkadia@gmail.com>
2024-10-14 16:01:33 +0530

    aloops

---
+++ Project_0/Control Statements/loops.py
@@ -0,0 +1,33 @@
+"""
+Loop: Entry-Controlled Loop
+
+1. for: fixed iteration
+syntax:
+    for iterator_var in iterable:
+        # body
+
+2. while: unfixed iteration
+syntax:
+    while condition:
+        # body
+
+"""
+
+# range(start=0, stop-1, step=1) function
+
+# for i in range(10):
+#     print(i, end=", ")
+# print("\b\b ")
+
+# user_input = int(input("Enter the end: "))
+# for i in range(user_input):
+#     print(i, end=', ')
+
+# i = 1
+# while i <= 10:
+#     print(i, end=', ')
+#     i+=1
+
+######################################################################
+
+# to be continued...
